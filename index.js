f7809a125efdc9f9c757b7d3ea1df1a9d83a81c6
Justin Deal <justin.deal@zapier.com>
2016-03-02 15:32:05 -0600

    bump addressbar; add replace prop; start parsing url for onChange

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var addressbar = require('addressbar');
 var React = require('react');
+var URL = require('url-parse');
 
 module.exports = React.createClass({
   propTypes: {
     onChange: React.PropTypes.func.isRequired,
-    value: React.PropTypes.string.isRequired
+    value: React.PropTypes.string.isRequired,
+    replace: React.PropTypes.bool
   },
   componentWillMount: function () {
     addressbar.on('change', this.onUrlChange);
@@ -13,17 +15,29 @@ module.exports = React.createClass({
     addressbar.removeListener('change', this.onUrlChange);
   },
   componentDidUpdate: function () {
-    addressbar.value = this.props.value;
+    addressbar.value = {
+      value: this.props.value,
+      replace: !!this.props.replace
+    };
   },
   componentDidMount: function () {
-    addressbar.value = this.props.value;
+    addressbar.value = {
+      value: this.props.value,
+      replace: !!this.props.replace
+    };
   },
   onUrlChange: function (event) {
-    if (this.props.onlyHash && event.target.value.indexOf('#') === -1) {
+    var url = event.target.value;
+    if (this.props.onlyHash && url.indexOf('#') === -1) {
       return;
     }
     event.preventDefault();
-    this.props.onChange(event.target.value);
+    var urlObj = URL(url);
+    var origin = urlObj.protocol + '//' + urlObj.host;
+    this.props.onChange(url, {
+      origin: origin,
+      pathname: urlObj.pathname
+    });
   },
   render: function () {
     return null;
